e720d1852f5ac04f1015839f7972abce37b2bcaa
Rafał Stępniewski <vultur12@gmail.com>
2023-06-16 09:59:12 +0200

    classes ship and shot are records now

---
+++ src/main/java/pl/rstepniewski/sockets/game/Ship.java
@@ -0,0 +1,17 @@
+package pl.rstepniewski.sockets.game;
+
+import java.awt.*;
+import java.util.List;
+
+/**
+ * Created by rafal on 16.06.2023
+ *
+ * @author : rafal
+ * @date : 16.06.2023
+ * @project : Battleship
+ */
+public record Ship(List<Point> pozycja, int dlugosc) {
+    public Ship {
+        dlugosc = pozycja.size();
+    }
+}
--- src/main/java/pl/rstepniewski/sockets/game/Shot.java
+++ src/main/java/pl/rstepniewski/sockets/game/Shot.java
@@ -7,13 +7,8 @@ package pl.rstepniewski.sockets.game;
  * @date : 11.06.2023
  * @project : Battleship
  */
-public class Shot {
-
-    private final char row;
-    private final int column;
-
-    private Shot(char row, int column) {
-        this.row = Character.toUpperCase(row);
-        this.column = column;
+public record Shot(char row, int column) {
+    public Shot {
+        row = Character.toUpperCase(row);
     }
 }
